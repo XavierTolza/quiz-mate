9dc6caa47cce820f8f9f5d49d7ba61994dcaea06
Xavier TOLZA <xtolza@neverhack.com>
2025-04-09 13:44:20 +0200

    eable images on the presenter

--- backend/src/handlers.js
+++ backend/src/handlers.js
@@ -127,9 +127,7 @@ module.exports.onWebsocketConnect = (io, socket) => {
             roomObj.questionIndex = question.index;
             roomObj.questionStart = Date.now();
         }
-        // Strip image data before sending to players
-        const { imageUrl, ...playerQuestion } = question;
-        socket.to(roomCode).emit(commands.ANSWERS_OPEN, { ...playerQuestion, correct: undefined });
+        socket.to(roomCode).emit(commands.ANSWERS_OPEN, { ...question, correct: undefined });
     });
 
     //------------------------------------------------------------------------------------------------------------------
